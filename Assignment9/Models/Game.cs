08bbc2ea42daa323816447dc64236876984e188b
Julian Egbert <jjegbe01@louisville.edu>
2015-03-25 11:19:59 -0400

    Added project files.

---
+++ Assignment9/Models/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Assignment9.Models
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
+        public string esrbRating { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [StringLength(40)]
+        public string Developer { get; set; }
+
+        [StringLength(40)]
+        public string Publisher { get; set; }
+
+        public int Score { get; set; }
+    }
+
+    public class GameDBContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+}
